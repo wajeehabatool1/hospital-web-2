db45e91d9461d7ef8e6a9af39a430441094ece06
wajeeha batool <96997969+wajeehabatool1@users.noreply.github.com>
2024-11-19 02:33:22 -0800

    Update Greeting.js

--- frontend/src/Greeting.js
+++ frontend/src/Greeting.js
@@ -1,8 +1,21 @@
 // src/Greeting.js
 //import React from 'react';
 
+/*const Greeting = ({ name }) => {
+    return <h1>Hello, {name}!</h1>;
+};
+
+export default Greeting;*/
+
+import PropTypes from 'prop-types';
+
 const Greeting = ({ name }) => {
     return <h1>Hello, {name}!</h1>;
 };
 
+Greeting.propTypes = {
+    name: PropTypes.string.isRequired, // Validate 'name' as a required string
+};
+
 export default Greeting;
+
