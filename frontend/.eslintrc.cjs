2ee90396c6823fc254c784c2f12c836977846712
wajeeha batool <96997969+wajeehabatool1@users.noreply.github.com>
2024-11-19 21:13:26 -0800

    Update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -17,4 +17,43 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+}*/
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, // Added for backend compatibility (if necessary)
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended', // Add TypeScript ESLint plugin
+    'plugin:@typescript-eslint/recommended-requiring-type-checking', // Enables stricter rules
+  ],
+  parser: '@typescript-eslint/parser', // Set parser for TypeScript files
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Ensure JSX is supported
+    },
+  },
+  settings: {
+    react: {
+      version: '18.2', // Ensure correct React version
+    },
+  },
+  plugins: ['react-refresh', '@typescript-eslint'], // Add TypeScript plugin
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Example TypeScript rule
+  },
 }
+
